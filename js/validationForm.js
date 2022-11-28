1c511cb502e688b9f3cbfa417c374fb565ffaac4
Lisitius <90479443+Lisitius@users.noreply.github.com>
2022-11-28 10:17:18 +0100

    function checkEmail & check Birthdate & check Tournament ok

--- js/validationForm.js
+++ js/validationForm.js
@@ -1,7 +1,10 @@
 //get dom element
 const firstname = document.getElementById("first");
 const lastname = document.getElementById("last");
+const email = document.getElementById("email");
 const form = document.getElementById("form");
+const birthdate = document.getElementById("birthdate");
+const tournament = document.getElementById("quantity");
 
 //regex
 const emailRegex =
@@ -11,9 +14,10 @@ const emailRegex =
 function checkFirstname() {
   if (firstname.value.length < 2) {
     firstname.parentElement.setAttribute("data-error-visible", "true");
+    firstname.style.border = "2px solid #ff0000";
     return false;
   } else {
-    firstname.parentElement.setAttribute("data-error-visible", "false");
+    first.parentElement.setAttribute("data-error-visible", "false");
     firstname.style.border = "2px solid #7CFC00";
     return true;
   }
@@ -23,10 +27,55 @@ function checkFirstname() {
 function checkLastname() {
   if (lastname.value.length < 2) {
     lastname.parentElement.setAttribute("data-error-visible", "true");
+    lastname.style.border = "2px solid #ff0000";
     return false;
   } else {
     lastname.parentElement.setAttribute("data-error-visible", "false");
     lastname.style.border = "2px solid #7CFC00";
+    return true;
+  }
+}
+
+//function check email
+function checkEmail() {
+  if (email.value.match(emailRegex)) {
+    email.parentElement.setAttribute("data-error-visible", "false");
+    email.style.border = "2px solid #7CFC00";
+    return true;
+  } else {
+    email.parentElement.setAttribute("data-error-visible", "true");
+    email.style.border = "2px solid #ff0000";
+    return false;
+  }
+}
+
+//function birthdate
+function checkBirthdate() {
+  const date = new Date(birthdate.value);
+  if (date == "Invalid Date") {
+    birthdate.parentElement.setAttribute("data-error-visible", "true");
+    birthdate.style.border = "2px solid #ff0000";
+    return false;
+  } else {
+    birthdate.parentElement.setAttribute("data-error-visible", "false");
+    birthdate.style.border = "2px solid #7CFC00";
+    return true;
+  }
+}
+
+//function tournament
+function checkTournament() {
+  if (
+    tournament.value === "" ||
+    tournament.value < 0 ||
+    isNaN(tournament.value) === true
+  ) {
+    tournament.parentElement.setAttribute("data-error-visible", "true");
+    quantity.style.border = "2px solid #ff0000";
+    return false;
+  } else {
+    tournament.parentElement.setAttribute("data-error-visible", "false");
+    quantity.style.border = "2px solid #7CFC00";
     return true;
   }
 }
@@ -37,16 +86,27 @@ function fieldValidation(element, method, event) {
 }
 
 fieldValidation(firstname, checkFirstname, "focusout");
-fieldValidation(lastname, checkFirstname, "focusout");
+fieldValidation(lastname, checkLastname, "focusout");
+fieldValidation(email, checkEmail, "focusout");
+fieldValidation(birthdate, checkBirthdate, "focusout");
+fieldValidation(tournament, checkTournament, "focusout");
 
 function allFieldValidation() {
   checkFirstname();
   checkLastname();
+  checkEmail();
+  checkBirthdate();
+  checkTournament();
 }
 
 function validate() {
-  if (checkFirstname() === true && checkLastname() === true) {
-    console.log("field ok");
+  if (
+    checkFirstname() &&
+    checkLastname() &&
+    checkEmail() &&
+    checkBirthdate() &&
+    checkTournament() === true
+  ) {
     return true;
   }
   return false;
