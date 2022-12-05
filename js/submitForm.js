dd3f5d12fd3a620ba274f63a6826a879d2a52cd3
Lisitius <90479443+Lisitius@users.noreply.github.com>
2022-12-05 14:57:34 +0100

    reset field form and comment code

--- js/submitForm.js
+++ js/submitForm.js
@@ -2,16 +2,22 @@ const thanksModalBg = document.getElementsByClassName("thanks-modal-bg");
 const closeBtnThanksModal = document.getElementById("close-btn-thanks-modal");
 const closeThanksModal = document.getElementsByClassName("close-thanks-modal");
 
+//display thankModalBg in index.html
 function showThanksSubmit() {
   modalbg.style.display = "none";
   thanksModalBg[0].style.display = "block";
 }
 
+// close popup and reset border style
 function closeSubmit() {
   thanksModalBg[0].style.display = "none";
   first.style.border = "none";
   last.style.border = "none";
+  email.style.border = "none";
+  birthdate.style.border = "none";
+  tournament.style.border = "none";
 }
 
+//button for close popup
 closeThanksModal[0].addEventListener("click", closeSubmit);
 closeBtnThanksModal.addEventListener("click", closeSubmit);
--- js/validationForm.js
+++ js/validationForm.js
@@ -1,4 +1,4 @@
-//get dom element
+//get form fields
 const firstname = document.getElementById("first");
 const lastname = document.getElementById("last");
 const email = document.getElementById("email");
@@ -17,11 +17,14 @@ const emailRegex =
 
 //function check firstname
 function checkFirstname() {
+  // checks if first name has less than 2 characters
   if (firstname.value.length < 2) {
+    //set data-error true if firstname is not valid and add red border
     firstname.parentElement.setAttribute("data-error-visible", "true");
     firstname.style.border = "2px solid #ff0000";
     return false;
   } else {
+    //set data-error false if firstname is valid and add green border
     first.parentElement.setAttribute("data-error-visible", "false");
     firstname.style.border = "2px solid #7CFC00";
     return true;
@@ -30,11 +33,14 @@ function checkFirstname() {
 
 //function check lastname
 function checkLastname() {
+  // checks if last name has less than 2 characters
   if (lastname.value.length < 2) {
+    //set data-error true if lastname is not valid and add red border
     lastname.parentElement.setAttribute("data-error-visible", "true");
     lastname.style.border = "2px solid #ff0000";
     return false;
   } else {
+    //set data-error false if lastname is valid and add green border
     lastname.parentElement.setAttribute("data-error-visible", "false");
     lastname.style.border = "2px solid #7CFC00";
     return true;
@@ -43,11 +49,14 @@ function checkLastname() {
 
 //function check email
 function checkEmail() {
+  // check if the email is valid with the help of regex
   if (email.value.match(emailRegex)) {
+    // set data-error false if the email is valid and add green border
     email.parentElement.setAttribute("data-error-visible", "false");
     email.style.border = "2px solid #7CFC00";
     return true;
   } else {
+    // set data-error true if the email is not valid and add red border
     email.parentElement.setAttribute("data-error-visible", "true");
     email.style.border = "2px solid #ff0000";
     return false;
@@ -56,8 +65,9 @@ function checkEmail() {
 
 //function birthdate
 function checkBirthdate() {
+  //check if birthdate have a value
   const date = new Date(birthdate.value);
-  if (date == "Invalid Date") {
+  if (date === "Invalid Date") {
     birthdate.parentElement.setAttribute("data-error-visible", "true");
     birthdate.style.border = "2px solid #ff0000";
     return false;
@@ -70,35 +80,33 @@ function checkBirthdate() {
 
 //function tournament
 function checkTournament() {
+  //check if tournament is empty or has a value less than or if it is not a number
   if (
     tournament.value === "" ||
     tournament.value < 0 ||
     isNaN(tournament.value) === true
   ) {
+    //set data-error true if tournament is not valid and add red border
     tournament.parentElement.setAttribute("data-error-visible", "true");
     quantity.style.border = "2px solid #ff0000";
     return false;
   } else {
+    // set data-error false if tournament is valid and add green border
     tournament.parentElement.setAttribute("data-error-visible", "false");
     quantity.style.border = "2px solid #7CFC00";
     return true;
   }
 }
 
-function checkboX() {
-  if (checkbox1.checked === true) {
-    checkbox1.parentElement.setAttribute("data-error-visible", "false");
-    return true;
-  } else {
-    checkbox1.parentElement.setAttribute("data-error-visible", "true");
-    return false;
-  }
-}
-
+//function tournament location
 function checkLocation() {
+  //set data-error to true
   locations.setAttribute("data-error-visible", "true");
+  //loop all locations for check if a location are checked
   for (let i = 0; i < checkboxInput.length; i++) {
+    //if one of a locations are check
     if (checkboxInput[i].checked) {
+      //set data error to false and return true
       locations.setAttribute("data-error-visible", "false");
       return true;
     }
@@ -106,19 +114,36 @@ function checkLocation() {
   return false;
 }
 
+//function checkbox terms of use
+function checkboX() {
+  //check if the checkbox is checked
+  if (checkbox1.checked === true) {
+    //set data-error false et return true if a checkbox is checked
+    checkbox1.parentElement.setAttribute("data-error-visible", "false");
+    return true;
+  } else {
+    //set data-error true if the checkbox are not checked
+    checkbox1.parentElement.setAttribute("data-error-visible", "true");
+    return false;
+  }
+}
+
 ////field valid
 function fieldValidation(element, method, event) {
   element.addEventListener(event, method);
 }
 
+//add a defocus of the inputs
 fieldValidation(firstname, checkFirstname, "focusout");
 fieldValidation(lastname, checkLastname, "focusout");
 fieldValidation(email, checkEmail, "focusout");
 fieldValidation(birthdate, checkBirthdate, "focusout");
 fieldValidation(tournament, checkTournament, "focusout");
+//add an event on checkbox
 fieldValidation(locations, checkLocation, "change");
 fieldValidation(checkbox1, checkboX, "change");
 
+//All field validation check
 function allFieldValidation() {
   checkFirstname();
   checkLastname();
@@ -129,7 +154,9 @@ function allFieldValidation() {
   checkboX();
 }
 
+//form validation check
 function validate() {
+  //if all function return true so return true else return false
   if (
     checkFirstname() &&
     checkLastname() &&
@@ -139,18 +166,20 @@ function validate() {
     checkboX() &&
     checkLocation() === true
   ) {
-    console.log("test");
     return true;
   }
   return false;
 }
 
+//form submit
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   if (validate() === true) {
+    //if function validate is true launch function showThanksSubmit in submitform.js and reset the form field
     showThanksSubmit();
-    console.log("test2");
+    document.querySelector("form").reset();
   } else {
+    //else go back in allFieldValidation
     allFieldValidation();
   }
 });
